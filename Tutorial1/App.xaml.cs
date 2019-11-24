529c03f30c4e202d56a13ce81c70d3ae726df672
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-24 12:42:01 +0000

    Stack Layout in XAML

--- Tutorial1/App.xaml.cs
+++ Tutorial1/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Tutorial1
         {
             InitializeComponent();
 
-            MainPage = new GreetPage();
+            MainPage = new StackPage();
         }
 
         protected override void OnStart()
