e3452eca09ee7ff1780f69c838c2a7026085a1c4
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-27 22:38:05 +0000

    Modal Pages

--- Tutorial1/Navigation/IntroductionPage.xaml.cs
+++ Tutorial1/Navigation/IntroductionPage.xaml.cs
@@ -19,7 +19,7 @@ namespace Tutorial1.Navigation
 
         async private void Button_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PopAsync();
+            await Navigation.PopModalAsync();
         }
 
         protected override bool OnBackButtonPressed()
--- Tutorial1/Navigation/WelcomePage.xaml.cs
+++ Tutorial1/Navigation/WelcomePage.xaml.cs
@@ -19,7 +19,7 @@ namespace Tutorial1.Navigation
 
         async private void Button_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new IntroductionPage());
+            await Navigation.PushModalAsync(new IntroductionPage());
         }
     }
 }
